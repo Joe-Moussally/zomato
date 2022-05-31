937008dc2848d0b24003635d164ff9f9702adec5
MJoe111 <joemoussally111@gmail.com>
2022-05-31 18:43:07 +0300

    Adding ids for see reviews

--- draft/scripts/see_reviews.js
+++ draft/scripts/see_reviews.js
@@ -1,4 +1,5 @@
 let url = 'http://foodity/backend/admincomments.php';
+let ul = $('#see-reviews-container').children('ul')[0];
 
 console.log(ul);
 
@@ -9,7 +10,7 @@ axios({
 }).then((response) => {
 
     for (let i=0; i<response.data.length;i++) {
-        $(ul).append('<li><div class="review-header"><div class="review-header-left"><img src="https://media.npr.org/assets/img/2022/04/18/gettyimages-1239860186-cc4ab75dba7fd565b66a36d4112c2e7a4209160d.jpg"><span>'+response.data[i].username+'</span></div><span>5<i class="fa-solid fa-star"></i></span></div><div class="review-body">'+response.data[i].review+'</div><div class="accept-reject"><button class="accept">Accept</button><button class="reject">Reject</button></div></li>')
+        ul.innerHTML += '<li><div class="review-header"><div class="review-header-left"><img src="https://media.npr.org/assets/img/2022/04/18/gettyimages-1239860186-cc4ab75dba7fd565b66a36d4112c2e7a4209160d.jpg"><span>'+response.data[i].username+'</span></div><span>5<i class="fa-solid fa-star"></i></span></div><div class="review-body">'+response.data[i].review+'</div><div class="accept-reject"><button class="accept">Accept</button><button class="reject">Reject</button></div></li>'
     }
 
     console.log(response.data)
