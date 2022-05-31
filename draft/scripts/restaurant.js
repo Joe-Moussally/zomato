19def75adb7d049484a28549eb8ac8cfc83b3e20
MJoe111 <joemoussally111@gmail.com>
2022-05-31 20:20:41 +0300

    Adding reviews

--- draft/scripts/restaurant.js
+++ draft/scripts/restaurant.js
@@ -1,63 +1,71 @@
-const restaurant_id =document.getElementById("restaurant_id");
-const description = document.getElementById("description");
-const cuisines = document.getElementById("cuisines"); 
-const review = document.getElementById("user-review-input");
-const reviews = document.getElementById("review-list"); 
-const user_id = window.localStorage.getItem('user_id');
-const resto_id = window.localStorage.getItem('resto_id');
+// const users =document.getElementById("users");
+// const restaurants = document.getElementById("restaurants");
+// const comments = document.getElementById("comments");
 
-        let data = {
-            users: users.value,
-            restaurants : restaurants.value,
-            comments :comments.value,
-        }
+    
 
-let url = './backend/backend/restaurant.php';
-        axios({
-            method: 'GET',
-            url: url,
-            params: data,
-        })     
-        .then(function (response) {
-            console.log(response)
-            let result = response.data;
-            let restaurant_id = restaurant_id;
-            let description = description;
-            let cuisines = cuisines;
-            let review = review;
-            let user_id = user_id;
-            let resto_id = resto_id;
+        // let data = {
+        //     users: users.value,
+        //     restaurants : restaurants.value,
+        //     comments :comments.value,
+        // }
 
-            users.innerHTML=usersphp;
-            restaurants.innerHTML=restaurantsphp;
-              comments.innerHTML=commentsphp;
-        }
+        // let url = 'http://foodity/backend/backend/statistics.php';
+        // axios({
+        //     method: 'GET',
+        //     url: url,
+        //     params: data,
+        // })     
+        // .then(function (response) {
+        //     console.log(response)
+        //     let result = response.data;
+        //     let usersphp = result[0].users;
+        //     let commentsphp = result[0].comments;
+        //     let restaurantsphp = result[0].restaurants;
+        //     users.innerHTML=usersphp;
+        //     restaurants.innerHTML=restaurantsphp;
+        //     comments.innerHTML=commentsphp;
 
-let url = './backend/backend/comment.php';
-let ul = $('#near-you-list').children('ul')[0];
-axios({
-    method: 'GET',
-    url: url
-})
-.then( (Response) => {
-    console.log(Response.data)
-    for (i=0; i<Response.data.length;i++) {
+            
 
-        ul.innerHTML += '<li onclick="redirect(event.currentTarget)" id="'+Response.data[i].id+'"><img src="'+Response.data[i].icon+'"><div class="card-body"><div class="card-header"><h2>'+Response.data[i].name+'</h2><span><i class="fa-solid fa-star"></i>4</span></div><span><i class="fa-solid fa-location-dot"></i>'+Response.data[i].location+'</span></div></li>'
-        
-    }
-});
-
-const redirect = (li) => {
-   
- 
-    let url2 = 'http://foodity/backend/restaurant.php';
-    url2 += '?resto_id='+li.id;
-    localStorage.setItem('resto_id',li.id);
-    window.location.replace('http://foodity/draft/restaurant.html');
-
-
-        });
+        // });
 
 // {"id":2,"name":"KFC","rating":0,"description":"test kfc
 // description","icon":null,"phone":"76069710","location":"test location kfc","cuisine":"chinese"}
+
+let resto_id = localStorage.getItem('resto_id');
+let cuisines = document.getElementById('cuisines')
+let resto_name = document.getElementById('restaurant_id')
+let resto_desc = document.getElementById('description')
+let phone = document.getElementById('phone')
+let resto_location = document.getElementById('location')
+
+let url = 'http://foodity/backend/restaurant.php'
+url += "?resto_id=" + resto_id;
+
+axios({
+        method: 'GET',
+        url: url
+    }).then((response) => {
+        console.log(response.data[0])
+        resto_name.innerHTML = response.data[0].name
+        resto_desc.innerHTML = response.data[0].description
+        phone.innerHTML = response.data[0].phone
+        resto_location.innerHTML = response.data[0].location
+    });
+
+//print restaurant reviews
+let url2 = 'http://foodity/backend/restaurantcomments.php'
+url2 += "?resto_id=" + resto_id;
+axios({
+        method: 'GET',
+        url: url2
+    }).then((Response) => {
+            console.log(Response.data)
+
+            let reviewlist = document.getElementById('review-list')
+            console.log(reviewlist)
+            for (let i=0;i<Response.data.length;i++) {
+                    reviewlist.innerHTML += '<li><div class="review-header"><div class="review-header-left"><img src="https://media.npr.org/assets/img/2022/04/18/gettyimages-1239860186-cc4ab75dba7fd565b66a36d4112c2e7a4209160d.jpg"><span>'+Response.data[i].username+'</span></div><span>5<i class="fa-solid fa-star"></i></span></div><div class="review-body">'+Response.data[i].review+'</div></li>'
+            }
+    })
